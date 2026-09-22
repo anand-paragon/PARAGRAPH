2559d66a79fdbead82ace5309178c5be43cf2baa
anand-paragon <15969483+anand-paragon@users.noreply.github.com>
2026-09-22 17:52:08 +0000

    ⤵️ Sync changes from remote

---
+++ src/integrations/imanage/config.ts
@@ -0,0 +1,18 @@
+import { IIntegrationConfig } from '@useparagon/core/integration';
+
+/**
+ * configuration for a imanage
+ */
+const config: IIntegrationConfig = {
+  description: 'Sync files and documents with iManage',
+  overviewText: `Connect to your iManage account to manage your files and documents in iManage. Increase your team’s productivity by keeping your iManage account up to date - without manual data entry.
+                      
+Our iManage integration enables you to:
+       
+• Save files and documents in iManage   
+• Sync files and documents from iManage`,
+  showWatermark: false,
+  workflowDisplayOrder: [],
+};
+
+export default config;
